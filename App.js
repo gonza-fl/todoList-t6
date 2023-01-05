a5116dbefdfd9f657cf04d65ae7f86128e26f21f
gonza.fl <gonzaloflamenci4@gmail.com>
2023-01-05 11:49:57 -0300

    Create structure directory, and form register with register and login screens

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import RegisterScreen from './src/screens/RegisterScreen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <RegisterScreen/>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ECFEFF',
   },
 });
