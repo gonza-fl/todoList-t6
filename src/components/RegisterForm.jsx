eeb379aa71bb74567cd85c76e659d80128025d64
UpmarketBat23 <75809408+UpmarketBat23@users.noreply.github.com>
2023-01-06 02:26:22 -0300

    ADD: Se agregaron los estilos de RegisterScreen (#3)

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -4,18 +4,30 @@ import {
   TextInput,
   TouchableOpacity,
   View,
-} from 'react-native';
-import React from 'react';
+} from "react-native";
+import React from "react";
 
 const RegisterForm = () => {
   return (
-    <View>
-      <TextInput style={styles.texInput} placeholder='Nombre completo' />
-      <TextInput style={styles.texInput} placeholder='Correo electronico' />
-      <TextInput style={styles.texInput} placeholder='Ingrese una contraseña' />
-      <TextInput style={styles.texInput} placeholder='Confirmar contraseña' />
-      <TouchableOpacity>
-        <Text>Registrar</Text>
+    <View style={styles.containerInput}>
+      <TextInput
+        style={styles.textInput}
+        placeholder='Ingrese su nombre completo'
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholder='Ingrese su correo electronico'
+      />
+      <TextInput
+        style={styles.textInput}
+        placeholder='Ingrese una contraseña'
+      />
+      <TextInput
+        style={[styles.textInput, styles.lastTextInput]}
+        placeholder='Confirmar la contraseña'
+      />
+      <TouchableOpacity style={styles.touchableOpacitybttn}>
+        <Text style={styles.txtBttn}>Registrarse</Text>
       </TouchableOpacity>
     </View>
   );
@@ -24,13 +36,33 @@ const RegisterForm = () => {
 export default RegisterForm;
 
 const styles = StyleSheet.create({
-  container: {
-    
+  containerInput: {
+    flexDirection: "column",
+    justifyContent: "space-evenly",
+    marginTop: 50,
   },
-  texInput: {
-    backgroundColor: '#ffffff',
-    width: 350,
+  textInput: {
+    backgroundColor: "#ffffff",
+    width: 300,
     borderRadius: 50,
     paddingHorizontal: 50,
+    flexBasis: 50,
+    margin: 8,
+  },
+  lastTextInput: {
+    marginBottom: 70,
+  },
+  firstTextInput: {},
+  touchableOpacitybttn: {
+    flexBasis: 50,
+    backgroundColor: "#58d0e0",
+    borderRadius: 19,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 20,
+  },
+  txtBttn: {
+    color: "#fff",
+    fontWeight: "bold",
   },
 });
