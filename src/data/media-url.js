4c26c682d1f5cfa837bc3287a272b83573c33923
gonza.fl <gonzaloflamenci4@gmail.com>
2023-01-06 02:15:55 -0300

    "Images deployed and create file with the urls"

---
+++ src/data/media-url.js
@@ -0,0 +1,6 @@
+export default {
+  elipse: 'https://i.postimg.cc/zG7fSfwj/elipse.png',
+  login: 'https://i.postimg.cc/wvvgkcxx/login.png',
+  onboarding: 'https://i.postimg.cc/XqHQDmHT/onboarding.png',
+  rectangle: 'https://i.postimg.cc/gc8BTLwG/rectangle.png',
+};
