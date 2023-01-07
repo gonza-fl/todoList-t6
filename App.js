aa1d51b218fbd57e12e3194be610eb2c52f35bfb
Gonza.fl <gonzaloflamenci4@gmail.com>
2023-01-07 17:38:05 -0300

    Add/navigation (#5)
    Install React Native Navigation and add navigation in to the code

--- App.js
+++ App.js
@@ -1,33 +1,35 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, View, Image } from "react-native";
-import RegisterScreen from "./src/screens/RegisterScreen";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, View, Image } from 'react-native';
+import RegisterScreen from './src/screens/RegisterScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './src/screens/LoginScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Image
-          source={require("./src/images/elipse.png")}
-          style={[styles.bgImage, styles.nose]}
-        />
-      </View>
-      <RegisterScreen />
-      <StatusBar style='auto' />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        {/* <StatusBar style='auto' /> */}
+        {/* <View>
+          <Image
+            source={require('./src/images/elipse.png')}
+            style={[styles.bgImage, styles.nose]}
+          />
+        </View> */}
+        {/* <View style={styles.container}> */}
+          <Stack.Screen name='Register' component={RegisterScreen} />
+          <Stack.Screen name='Login' component={LoginScreen} />
+        {/* </View> */}
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#e8e8e8",
-  },
-  bgImage: {
-    position: "absolute",
-    top: -100,
-    left: -100,
-    right: -100,
-    bottom: -100,
-    resizeMode: "contain",
+    backgroundColor: '#e8e8e8',
   },
 });
--- src/screens/RegisterScreen.jsx
+++ src/screens/RegisterScreen.jsx
@@ -1,10 +1,14 @@
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity, Image } from "react-native";
 import React from "react";
 import RegisterForm from "../components/RegisterForm";
 
-const RegisterScreen = () => {
+const RegisterScreen = ({navigation}) => {
   return (
     <View style={styles.container}>
+      <Image
+            source={require('../images/elipse.png')}
+            style={[styles.bgImage, styles.nose]}
+          />
       <Text style={[styles.textBold, styles.titulo]}>Bienvenido a Bordo!</Text>
       <Text>
         {" "}
@@ -12,10 +16,10 @@ const RegisterScreen = () => {
         Vamos ayudarte a cumplir con tus tareas
       </Text>
       <RegisterForm />
-      <View style={styles.lol}>
+      <View style={styles.footer}>
         <Text>¿Ya tienes una cuenta? </Text>
-        <TouchableOpacity>
-          <Text style={[styles.textBold, styles.color]}>Inicia sesión</Text>
+        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+          <Text style={[styles.textBold, styles.color]} >Inicia sesión</Text>
         </TouchableOpacity>
       </View>
     </View>
@@ -35,16 +39,21 @@ const styles = StyleSheet.create({
     marginTop: 120,
     marginBottom: 20,
   },
-  // subtitulo: {
-  //   marginBottom: 20,
-  // },
+  bgImage: {
+    position: 'absolute',
+    top: -100,
+    left: -100,
+    right: -100,
+    bottom: -100,
+    resizeMode: 'contain',
+  },
   textBold: {
     fontWeight: "bold",
   },
   color: {
     color: "#58d0e0",
   },
-  lol: {
+  footer: {
     flexDirection: "row",
   },
 });
