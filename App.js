16173e11c9a3019e15d541a2b4c09c0529fa2524
gonza.fl <gonzaloflamenci4@gmail.com>
2023-01-07 17:43:08 -0300

    Clear comments and fix style code in App.js

--- App.js
+++ App.js
@@ -1,5 +1,3 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View, Image } from 'react-native';
 import RegisterScreen from './src/screens/RegisterScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -11,25 +9,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {/* <StatusBar style='auto' /> */}
-        {/* <View>
-          <Image
-            source={require('./src/images/elipse.png')}
-            style={[styles.bgImage, styles.nose]}
-          />
-        </View> */}
-        {/* <View style={styles.container}> */}
           <Stack.Screen name='Register' component={RegisterScreen} />
           <Stack.Screen name='Login' component={LoginScreen} />
-        {/* </View> */}
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#e8e8e8',
-  },
-});
