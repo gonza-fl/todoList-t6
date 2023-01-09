8412fce4e126bd6f74e6e85aeec219d2ffb7de52
UpmarketBat23 <75809408+UpmarketBat23@users.noreply.github.com>
2023-01-09 18:30:37 -0300

    *➕ADD*:: Se crearon nuevos componentes (#7)

---
+++ src/components/InputForm.jsx
@@ -0,0 +1,21 @@
+import { TextInput, StyleSheet } from "react-native";
+
+import React from "react";
+
+const InputForm = (prop) => {
+  const { Placeholder } = prop;
+  return <TextInput style={styles.textInput} placeholder={Placeholder} />;
+};
+
+export default InputForm;
+
+const styles = StyleSheet.create({
+  textInput: {
+    backgroundColor: "#ffffff",
+    width: 300,
+    borderRadius: 50,
+    paddingHorizontal: 50,
+    flexBasis: 50,
+    margin: 8,
+  },
+});
---
+++ src/components/TODoButton.jsx
@@ -0,0 +1,28 @@
+import { StyleSheet, Text, TouchableOpacity } from "react-native";
+import React from "react";
+
+const TODoButton = (props) => {
+  const { text } = props;
+  return (
+    <TouchableOpacity style={styles.touchableOpacitybttn}>
+      <Text style={styles.txtBttn}>{text}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default TODoButton;
+
+const styles = StyleSheet.create({
+  touchableOpacitybttn: {
+    flexBasis: 50,
+    backgroundColor: "#58d0e0",
+    borderRadius: 19,
+    alignItems: "center",
+    justifyContent: "center",
+    marginBottom: 10,
+  },
+  txtBttn: {
+    color: "#fff",
+    fontWeight: "bold",
+  },
+});
