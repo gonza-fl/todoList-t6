01e8763c91a9a61d22275fec068f8a348c476463
gonza.fl <gonzaloflamenci4@gmail.com>
2023-01-10 02:39:19 -0300

    add react version auto in eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,11 @@ module.exports = {
     es2021: true,
     node: true,
   },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
   extends: [
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
