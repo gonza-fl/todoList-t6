03c67d612119705e32cf37f03e64f2e8f2a4899f
uper23 <xxxnaujxxx@gmail.com>
2023-01-10 03:17:03 -0300

    *➕ADD*:Se acreo el componente Task

--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -1,5 +1,5 @@
 import { StyleSheet, View } from 'react-native';
-import ToDoButton from './ToDoButton';
+import ToDoButton from './TODoButton';
 import InputForm from './InputForm';
 
 const RegisterForm = () => {
