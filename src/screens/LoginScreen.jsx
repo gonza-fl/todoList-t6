cbe7c84630c136c83b52e95b77998fa6459e203b
karlacosta1 <120480696+karlacosta1@users.noreply.github.com>
2023-01-11 12:14:17 -0300

    loginform actualizado (#11)

--- src/screens/LoginScreen.jsx
+++ src/screens/LoginScreen.jsx
@@ -1,11 +1,46 @@
-import { Text, View } from 'react-native';
+import { StyleSheet, View, Text, Image, TouchableOpacity } from 'react-native';
+import { login } from '../data/media-url';
+import React from 'react'
+import LoginForm from '../components/LoginForm';
 
-const LoginScreen = () => {
+const LoginScreen = ({ navigation }) => {
   return (
-    <View>
-      <Text>LoginScreen</Text>
-    </View>
-  );
-};
+    <View style={styles.container}>
+     <Text style={[styles.textBold, styles.titulo]}>¡Bienvenido de vuelta!</Text>
+     <Image source={{ uri: login }} style={[styles.bgImage, styles.nose]} />
+    <LoginForm/>
+    <Text> ¿Olvidaste tu contraseña? </Text>
+    <TouchableOpacity onPress={() => navigation.navigate('Register')}>
+          <Text style={[styles.textBold, styles.color]}>Recuperar contraseña</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  };
 
-export default LoginScreen;
+export default LoginScreen
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  titulo: {
+    fontSize: 30,
+    marginTop: 20,
+    marginBottom: 80,
+  },
+  textBold: {
+    fontWeight: 'bold',
+  },
+  color: {
+    color: '#58d0e0',
+  },
+  bgImage: {
+    position: 'absolute',
+    top: 10,
+    left: 10,
+    right: 10,
+    bottom: 10,
+    resizeMode: 'contain',
+  },
+});
