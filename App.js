616139af1c43b05e4673e6478e7e02d652ba5dd4
gonza.fl <gonzaloflamenci4@gmail.com>
2023-01-12 09:41:16 -0300

    funtion get all task and screen to view task list

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import RegisterScreen from './src/screens/RegisterScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './src/screens/LoginScreen';
+import ToDoListScreen from './src/screens/ToDoListScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,8 +10,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name='Register' component={RegisterScreen} />
-        <Stack.Screen name='Login' component={LoginScreen} />
+        {/* <Stack.Screen name='Register' component={RegisterScreen} />
+        <Stack.Screen name='Login' component={LoginScreen} /> */}
+        <Stack.Screen name='ToDoList' component={ToDoListScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- src/data/urls.js
+++ src/data/urls.js
@@ -1,4 +1,6 @@
 const BASE_URL = 'http://192.168.1.19:3000';
 export const URL = {
   LOGIN: `${BASE_URL}/user/login`,
+  REGISTER: `${BASE_URL}/user/register`,
+  TASK: `${BASE_URL}/task`,
 };
