a8d236d0cefcc1c8ce8cd3d944e602519dc91908
gonza.fl <gonzaloflamenci4@gmail.com>
2023-01-13 09:37:21 -0300

    reagister integration is redeady

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import RegisterScreen from './src/screens/RegisterScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './src/screens/LoginScreen';
+import { LogBox } from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
+  LogBox.ignoreLogs(['Remote debugger']);
   return (
     <NavigationContainer>
       <Stack.Navigator>
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -34,7 +34,7 @@ const LoginForm = () => {
           Placeholder='Ingrese su contraseña'
           handleChangeText={setPassword}
           value={password}
-          isSecure={true}
+          isHiden={true}
         />
       </View>
       <ToDoButton text='Iniciar Sesion' handleOnpress={login} />
