3dd03f8a25bff3fcec59149c26228ce4aa4fbc52
danielaescandar <danielaescandar@gmail.com>
2023-01-14 18:21:19 -0300

    se agrega en App.js la navegación a GetStarted

--- App.js
+++ App.js
@@ -2,13 +2,20 @@ import RegisterScreen from './src/screens/RegisterScreen';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './src/screens/LoginScreen';
+import GetStartedScreen from './src/screens/GetStartedScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+          initialRouteName='GetStarted'
+          screenOptions={{
+          headerShown:false,
+         }}
+      >
+        <Stack.Screen name='GetStarted' component={GetStartedScreen} />
         <Stack.Screen name='Register' component={RegisterScreen} />
         <Stack.Screen name='Login' component={LoginScreen} />
       </Stack.Navigator>
