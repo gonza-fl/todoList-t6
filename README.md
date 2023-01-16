e49082e7a4a3764c9ab179be7dba70f162d1fa73
danielaescandar <68254254+danielaescandar@users.noreply.github.com>
2023-01-16 11:22:55 -0300

    Add/screen get started (#23)
    Se crea el screen GetStarted en GetStartedScreen.jsx y se crea el archivo README.md

---
+++ README.md
@@ -0,0 +1,26 @@
+# APP ToDo List
+Aplicación móvil para realizar listas de tareas utilizando React-Native y Expo Cli.
+
+![](https://media.istockphoto.com/id/1288486122/es/vector/concepto-de-encuesta-en-l%C3%ADnea-personajes-masculinos-y-femeninos-peque%C3%B1os-que-llenan-el.jpg?s=612x612&w=0&k=20&c=05pVwnTZIgRP_8MdoXWfeGId9fgfsooEb-eKNDxMI4U=)
+
+
+## Características
+- Crear listas de tareas
+- Introducir una descripción por tarea
+- Indicar estado de la misma
+- Editar o eliminar tareas
+- Crear múltiples usuarios
+___
+
+## Clonar e instalar
+- Clonar este repositorio
+    - `git clone`  https://github.com/gonza-fl/team6-project
+- `cd Todo`
+* Correr `npm install`
+* Correr `npm start`
+* Presione `i` para ejecutar en el simulador de iOS o presione `a` para Android
+### Dispositivo móvil
+* Descarga la aplicación Expo
+* Escanee el código QR desde el móvil o abra la pestaña de exploración, presione buscar e ingrese la URL que se muestra en la terminal.
+___
+## Colaboradores
---
+++ src/screens/GetStartedScreen.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import { useNavigation } from '@react-navigation/native';
+import { StyleSheet, Text, View, TouchableOpacity, Image } from 'react-native';
+import images from '../configs/media-url'
+import TODoButton from '../components/TODoButton';
+
+
+
+const GetStartedScreen =() =>{
+
+    const navigation= useNavigation();
+
+    return(
+        <View style={styles.container}>
+             <Image source={{ uri:images.elipse }} style={styles.bgImage} />
+             <Image source={{uri: images.onboarding}} style={styles.getImage}/>
+            <View style={styles.content}>
+           
+             <Text style={[styles.textBold, styles.titulo]}>Hacé tus cosas con ToDo</Text>
+             <Text style={styles.description}>Comienza a crear tus listas de tareas para mantenerte organizado.{'\n'}Si estás listo presiona el siguiente botón... </Text>
+            
+             <TODoButton text='Comenzar' onPress={() => navigation.navigate('Login')} />
+            </View>
+             
+        </View>
+    )
+
+}
+export default GetStartedScreen;
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    content: {
+      flexDirection: 'column',
+      justifyContent: 'space-evenly',
+      marginTop: 30,
+    },
+    titulo: {
+      fontSize: 28,
+      top:90,
+      marginTop: 80,
+      marginBottom: 20,
+    },
+    bgImage: {
+      width: 250,
+      position: 'absolute',
+      top: -100,
+      left: -70,
+      right: 0,
+      bottom: 700,
+      resizeMode: 'contain',
+    },
+    getImage:{
+      width:50,
+      position:'absolute',
+      resizeMode: 'contain',
+      width:180,
+      top: -180,
+      left: 120,
+      right: -100,
+      bottom: -100,
+      marginBottom: 200,
+    },
+    description: { 
+     textAlign: 'center',
+     width:340,
+     top:80,
+     marginBottom: 200,
+     justifyContent: 'center',
+     alignItems:'center',
+     
+    },
+    textBold: {
+      fontWeight: 'bold',
+    },
+    color: {
+      color: '#58d0e0',
+    },
+      txtBttn: {
+        color: '#fff',
+        fontWeight: 'bold',
+      },
+  });
