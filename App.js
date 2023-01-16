6acaaaceedfc5944fbf9a4668a77c52b97d74ed4
Gonza.fl <gonzaloflamenci4@gmail.com>
2023-01-16 12:25:36 -0300

    Add/update task state (#24)
    add function and refactor in change status of the card and  fix GetStarted imports and function reference

--- App.js
+++ App.js
@@ -3,6 +3,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from './src/screens/LoginScreen';
 import GetStartedScreen from './src/screens/GetStartedScreen';
+import { LogBox } from 'react-native';
+import ToDoListScreen from './src/screens/ToDoListScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -11,11 +13,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator
-          initialRouteName='GetStarted'
-          screenOptions={{
+        initialRouteName='GetStarted'
+        screenOptions={{
           headerShown: false,
-        }}
-      >
+        }}>
         <Stack.Screen name='GetStarted' component={GetStartedScreen} />
         <Stack.Screen name='Register' component={RegisterScreen} />
         <Stack.Screen name='Login' component={LoginScreen} />
